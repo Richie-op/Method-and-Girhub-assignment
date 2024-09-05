9350018ebb05c744134dfe297dde8401903fec5a
richardsonnoel01 <richardsonnoel01@gmail.com>
2024-09-05 01:34:48 -0400

    calculated Risk

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+totalCalculatedRisk();
+    }
+
+    public static void calculateCrimeRisk() {
+        System.out.println("75");
+    }
+
+    public static void calculateEarthquakeRisk() {
+        System.out.println("25");
+    }
+
+    public static void calculateFloodRisk() {
+        System.out.println("23");
+    }
+
+    public static void totalCalculatedRisk() {
+        calculateCrimeRisk();
+        calculateEarthquakeRisk();
+        calculateFloodRisk();
+    }
+
+}
